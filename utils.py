7d34aef0c3aaa6f2b749714dbc93d6f061457b62
Marius Thomsen <mariu@itu.dk>
2025-03-21 12:43:50 +0100

    Utils set up

---
+++ utils.py
@@ -0,0 +1,137 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import networkx as nx
+
+#torch imports 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader,TensorDataset
+from torcheeg.io.eeg_signal import EEGSignalIO
+from torcheeg.models import DGCNN
+from torcheeg.models.gnn.dgcnn import GraphConvolution
+from sklearn.model_selection import train_test_split
+from torcheeg import transforms
+from torcheeg.models.gnn.dgcnn import GraphConvolution
+from cka import HookManager, CKACalculator
+
+
+#Matrix 
+def threshold(mat, thresh=0.2):
+    """ 
+    Helper function for get_adj_mat, Sets all entries in a matrix below the threshold to 0
+
+    ...
+
+    Parameters
+    -----
+    mat : two numpy arrays as a matrix
+        Torch.tensor as an matrix for numpy to cut off all low values
+    thresh : float
+        Threshold for the cut off
+
+    returns
+    -----
+    mat : two numpy arrays as a matrix
+        returns the matrix it recieved but with all  values below the thresh set to zero
+    """
+    mat[mat < thresh] = 0.0
+    return mat
+
+def get_adj_mat(model, thresh=0.2):
+    """ 
+    Extracts the adjacency matrix from a DGCNN model and normalise it, cutting noise
+
+    ...
+
+    Parameters
+    -----
+    model : DGCNN   
+        nural network model with a learned adjecency matrix
+    thresh : float
+        Threshold for the cut off
+
+    Returns
+    ----- 
+     A : two numpy arrays as a matrix
+        The adjacency matrix from the network
+    """
+    A=F.relu(model.A)
+    N=A.shape[0]
+    A=A*(torch.ones(N,N)-torch.eye(N,N))
+    A=A+A.T
+    A=threshold(A.detach(),thresh)
+    return A
+
+
+# Networkx utils
+def check_isomorphism(adj1, adj2):
+    
+    G1 = nx.from_numpy_array(adj1.numpy())
+    G2 = nx.from_numpy_array(adj2.numpy())
+    
+    return nx.vf2pp_is_isomorphic(G1, G2, node_label=None)
+
+
+#visualisation tools
+def plot_matrix(title,matrix_data,xlabel,ylabel,cbarlabel="",cellvalues=True):
+    """
+    Used for plotting our matrixis to unsure they are all the same format.
+
+    ...
+
+    Parameters
+    ------------
+    title : string
+        The title on the plot
+    matrix_data : np.ndarray
+        The data for creating the heatmap
+    xlabel : list
+        A list of strings for the label marks
+    ylabel : list
+        A list of strings for the label marks
+    cbarlabel : string
+        default = "", set if cbar name is needed
+    cellvalues : bool
+        default = true, if fase will stop showing the valies for each matrix cell
+    
+    """
+    plt.figure(figsize=(10, 10))
+    plt.imshow(matrix_data,cmap='inferno',vmin=0,vmax=1)
+    plt.xticks(ticks=np.arange(len(xlabel)), labels=xlabel)
+    plt.yticks(ticks=np.arange(len(ylabel)), labels=ylabel)
+    # Colourbar, with fixed ticks to enable comparison
+        # plt.colorbar(label="Edge Strength")
+
+    cbar = plt.colorbar(label=cbarlabel)
+    cbar.set_ticks([x/20 for x in range(0,21)])
+    if cellvalues:
+        for i in range(matrix_data.shape[0]):  
+            for j in range(matrix_data.shape[1]):
+                plt.text(j, i, f"{matrix_data[i, j]:.2f}", ha='center', va='center', color='white' if matrix_data[i, j] < 0.5 else 'black')
+
+    plt.title(title)
+    ax = plt.gca()  
+    ax.set_xticks(np.arange(len(xlabel)) - 0.5, minor=True)
+    ax.set_yticks(np.arange(len(ylabel)) - 0.5, minor=True)
+    ax.grid(which="minor", color="Black", linewidth=0.5)
+    ax.tick_params(which="minor", size=0) 
+    plt.show()
+
+
+def visualize_adj_mat(adj_mat):
+    """"
+    Implements plot matrix sepcifically to make an adjacency matrix
+
+    ...
+
+    Parameters
+    -----
+        adj_mat : torch.Tensor
+            Tensor matrix representing an adjacency matrix
+    """
+    num_nodes = adj_mat.shape[0]
+    node_labels = np.arange(1, num_nodes + 1)
+    plot_matrix("Adjecency matrix",adj_mat,node_labels,node_labels,cbarlabel="Edge strength",cellvalues=False)
