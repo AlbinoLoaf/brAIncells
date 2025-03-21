517e4f28dca575a871aae81d295be687a261d4b6
keli1111 <ptvoturilol@gmail.com>
2025-03-21 14:03:59 +0100

    add more graph functions and docstrings to utils

--- utils.py
+++ utils.py
@@ -32,7 +32,7 @@ def threshold(mat, thresh=0.2):
     thresh : float
         Threshold for the cut off
 
-    returns
+    Returns
     -----
     mat : two numpy arrays as a matrix
         returns the matrix it recieved but with all  values below the thresh set to zero
@@ -65,9 +65,46 @@ def get_adj_mat(model, thresh=0.2):
     A=threshold(A.detach(),thresh)
     return A
 
+def get_barycenter(adj):
+    """
+    Calculates barycenter of graph - the node which minimizes its distance from all other nodes
+    AKA the center of the graph
+    If multiple nodes tie for the smallest distance, then all of them are returned (as a list)
+    
+     Parameters
+    -----------
+    adj: torch matrix
+        Adjacency matrix of graph
+        
+    Returns
+    -----
+    bar: list of ints
+        The node id(s) that minimize the distance to all other nodes
+    """
+    
+    G = nx.from_numpy_array(adj.numpy())
+    bar = nx.barycenter(G)
+    return bar
+
 
 # Networkx utils
 def check_isomorphism(adj1, adj2):
+    """
+    Check if two graphs are isomorphic ie structurally the same
+    
+    Parameters
+    -----------
+    adj1: torch matrix
+        Adjacency matrix of first graph
+    adj2: torch matrix
+        Adjacency matrix of second graph
+        
+    Returns
+    -----
+    bool
+        Whether the two graphs are isomorphic
+    """
+
     
     G1 = nx.from_numpy_array(adj1.numpy())
     G2 = nx.from_numpy_array(adj2.numpy())
@@ -75,13 +112,60 @@ def check_isomorphism(adj1, adj2):
     return nx.vf2pp_is_isomorphic(G1, G2, node_label=None)
 
 def get_graph_edit_dist(adj1, adj2):
+    """
+    Get graph edit distance of two graphs
+    
+    Parameters
+    -----------
+    adj1: torch matrix
+        Adjacency matrix of first graph
+    adj2: torch matrix
+        Adjacency matrix of second graph
+        
+    Returns
+    -----
+    g_dist: float
+        Graph edit distance of the two graphs
+
+    """
     G1 = nx.from_numpy_array(adj1.numpy())
     G2 = nx.from_numpy_array(adj2.numpy())
     g_dist = nx.graph_edit_distance(G1, G2)
     return g_dist
 
+def get_simrank_similarity(adj):
+    """
+    Get simrank similarity of nodes in a graph
+    
+    Parameters
+    -----------
+    adj: torch matrix
+        Adjacency matrix of graph
+        
+    Returns
+    -----
+    dict
+        Simrank similarity of each pair of two nodes
+    """
+    
+    G = nx.from_numpy_array(adj.numpy())
+    return nx.simrank_similarity(G)
+
 
 def get_activations(model,data, layer_type=[GraphConvolution]):
+    """
+    Get model activations from specified types of layers
+    
+    Parameters
+    -----------
+    model: torch.nn model
+    layer_type: list of torch.nn layers
+    
+    Returns
+    -----
+    activations: torch FloatTensor
+        Extracted activations from specified layers
+    """
     
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = model.to(device).eval()
